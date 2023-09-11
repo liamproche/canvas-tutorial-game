638ca10d703ca692e02006953efe975f451e48b8
mac <mac@WDWWW.local>
2023-09-11 17:56:25 -0400

    added canvas to html and basic positioning to css

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 window.addEventListener('load', function(){
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 1280;
-    canvas.height = 720;
+    const canvas = document.getElementById('canvas1')
+    const ctx = canvas.getContext('2d')
+    canvas.width = 1280
+    canvas.height = 720
+    
+    class Player{
+        constructor(){
+            this.game = game
+        }
+    }
 
-} )
+    class Game{
+        constructor(canvas){
+            this.canvas = canvas
+            this.width = this.canvas.width
+            this.height = this.canvas.height
+            this.player = new Player(this)
+        }
+    }
+
+    const game = new Game(canvas)
+    console.log(game)
+
+    function animate(){
+
+    }
+
+})
